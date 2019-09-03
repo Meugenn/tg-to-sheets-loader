7a14b71547b948d8e01a0a9fb158d72ed5ac2756
Meugen <meugenn.neg@gmail.com>
2019-09-03 20:00:19 +0300

    added site info

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import os
 
 bot = telebot.TeleBot(const.bot_token)
 server = Flask(__name__)
+errors_text = ''
 
 list_of_subjects = get_subjects()
 list_of_subjects_in_low = {}
@@ -27,27 +28,29 @@ def start(message):
 
 @bot.message_handler(content_types=['text'])
 def subject_updater(message):
-    if re.match(list_for_regexp.lower(), message.text.lower()):
-        subject = re.match(list_for_regexp.lower(), message.text.lower()).group()
-        subject = list_of_subjects_in_low[subject]
-        text_homework = message.text[len(subject)+1:]
-        if text_homework[0] == ' ':
-            text_homework = text_homework[1:]
-        spreadsheet.update_homework(subject, text_homework)
-        bot.reply_to(message, 'Отлично, твое дз сохранено!')
-
+    try:
+        if re.match(list_for_regexp.lower(), message.text.lower()):
+            subject = re.match(list_for_regexp.lower(), message.text.lower()).group()
+            subject = list_of_subjects_in_low[subject]
+            text_homework = message.text[len(subject)+1:]
+            if text_homework[0] == ' ':
+                text_homework = text_homework[1:]
+            spreadsheet.update_homework(subject, text_homework)
+            bot.reply_to(message, 'Отлично, твое дз сохранено!')
+    except Exception as e:
+        global errors_text
+        errors_text += str(e) + '\n'
 
 @server.route('/' + const.bot_token, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://dzxa-loader.herokuapp.com/' + const.bot_token)
-    return "!", 200
+    return '\n'.join(list_of_subjects) + '\n' + errors_text, 200
 
 
 server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
